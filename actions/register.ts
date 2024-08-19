88c38a88e5fa42911153c5057975ad2e097b37f3
ALBERT <ishengomaalbert@gmail.com>
2024-08-19 09:52:48 +0300

    Register Functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 import * as z from "zod";
+import  bcrypt from "bcryptjs";
+import prisma from "@/lib/prisma";
 import { RegisterSchema } from "../schemas";
+import { getUserByEmail } from "../data/user";
 
 export const register = async (values:z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -9,5 +12,28 @@ export const register = async (values:z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success:"Email sent!"};
+  //Hash password
+  const {name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password,10);
+
+  //Check if email exist
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!"};
+  }
+
+  //Create a user
+  await prisma.user.create({
+    data:{
+      name,
+      email,
+      hashedPassword
+    }
+  })
+
+  //TODO: Send verification token email
+
+
+  return { success:"User created!"};
 }
