1ec95b6d5339acf1474c74596c877bd5ea35e6fa
ALBERT <ishengomaalbert@gmail.com>
2024-08-24 07:12:51 +0300

    Login & Logout functionality

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,4 +1,5 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 import { Metadata } from "next";
 
 export const metadata:Metadata = {
@@ -11,6 +12,16 @@ export default async function SettingsPage(){
         <main className="mx-auto my-10 space-y-4">
             <h1 className="text-center text-xl font-bold">Settings Page</h1>
              <div className="text-center">{JSON.stringify(session)}</div>
+
+             <div className="text-center my-8">
+               <form action={async () => {
+                 "use server";
+
+                 await signOut();
+               }}>
+                  <Button type="submit">Logout</Button>
+               </form>
+             </div>
         </main>
     )
 }
--- src/auth/index.ts
+++ src/auth/index.ts
@@ -3,19 +3,40 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
 import prisma from "@/lib/prisma";
 import Credentials from "next-auth/providers/credentials"
+import { LoginSchema } from "../../schemas";
+import { getUserByEmail } from "../../data/user";
+import bcrypt from "bcryptjs";
 
 
-
-export const BASE_PATH = "/api/auth";
 
 const authOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     Credentials({
+      async authorize(credentials){
+         const validatedFields = LoginSchema.safeParse(credentials);
+
+         if (validatedFields.success) {
+          const {email, password} = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+
+          if (!user || !user.hashedPassword) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.hashedPassword);
+
+          if (passwordsMatch) return user;
+         }
+
+         return null;
+      }
     }),
   ],
-  basePath: BASE_PATH,
   secret: process.env.AUTH_SECRET,
+  session: {
+     strategy: 'jwt' as 'jwt'
+  },
+
 };
 
 export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
